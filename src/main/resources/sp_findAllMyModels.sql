1c3292018f101ac0efaedb68e75240d16dc6ea98
Jose Iriarte <iriarte.joselito@gmail.com>
2019-06-25 03:15:04 -0400

    edit sql de SP for Save

--- src/main/resources/sp_findAllMyModels.sql
+++ src/main/resources/sp_findAllMyModels.sql
@@ -28,12 +28,23 @@ CREATE PROCEDURE GetAllImageFile
 AS
     SELECT * FROM image_file;
     GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
 
-CREATE PROCEDURE SaveBill
-    @bill_code nvarchar(50)
-    ,@is_deleted bit
+/***********************************************************************************************/
+CREATE PROCEDURE [dbo].[SaveBill]
+    @billCode nvarchar(50)
+    ,@isDeleted BIT
     ,@nit nvarchar(50)
-    ,@total_pay int
+    ,@totalPay int
+    ,@createdAt DATE
+    ,@version int
 
 AS
-    insert into bill (bill_code,is_deleted,nit,total_pay) values( @bill_code,@is_deleted,@nit,@total_pay )
+    insert into bill (bill_code,is_deleted,nit,total_pay,created_at,version) values( @billCode,@isDeleted,@nit,@totalPay,@createdAt,@version )
+    (select * from Bill where id = SCOPE_IDENTITY())
+    return
+GO
+
